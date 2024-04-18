a16cb2c3224ad3318043954961a57317becb00ea
isisgoncalves <isisvow@gmail.com>
2024-04-18 20:14:40 -0300

    Initial commit

---
+++ promise.js
@@ -0,0 +1,10 @@
+/*enunciado: criar uma Promise com um manipulador de tempo com o intervalo de 5 segundos;
+o objetivo da criação desta Promise é proporcionar à 
+aplicação a execução de três tarefas especificas - a seguir:
+
+a) criar um loop que exibirá um intervalo de valores que vai de 1 até 20;
+
+b) criar um novo loop que imprime os intervalos de números impares menores do que 100;
+
+c) imprimir a tabuada do número 9;
+*/
